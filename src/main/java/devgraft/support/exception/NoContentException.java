970941c6c69d42ef46eea17927e3ec878d9faec1
PCloud <pcloud63514@gmail.com>
2022-09-26 21:27:09 +0900

    Clear

--- src/main/java/devgraft/support/exception/NoContentException.java
+++
@@ -1,9 +0,0 @@
-package devgraft.support.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class NoContentException extends AbstractRequestException {
-    public NoContentException(final String message) {
-        super(message, HttpStatus.NO_CONTENT);
-    }
-}
