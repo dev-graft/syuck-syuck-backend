588644467a86de73a76a1758b3037e38a42f7472
PCloud <pcloud63514@gmail.com>
2022-10-14 19:42:42 +0900

    Added: Follow Doc - 팔로우 요청 - 팔로우 취소

--- src/test/java/devgraft/docs/follow/CancelFollowApiDoc.java
+++ src/test/java/devgraft/docs/follow/CancelFollowApiDoc.java
@@ -3,18 +3,16 @@ package devgraft.docs.follow;
 import devgraft.auth.api.AuthCodeFilter;
 import devgraft.common.credential.MemberCredentials;
 import devgraft.common.credential.MemberCredentialsResolver;
-import devgraft.follow.api.AskFollowApi;
+import devgraft.follow.api.CancelFollowApi;
 import devgraft.follow.app.AskFollowRequest;
-import devgraft.follow.app.AskFollowService;
+import devgraft.follow.app.CancelFollowService;
 import devgraft.support.restdocs.AbstractApiDocTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.payload.JsonFieldType;
 
 import javax.servlet.http.Cookie;
 
@@ -23,37 +21,36 @@ import static devgraft.common.URLPrefix.FOLLOW_URL_PREFIX;
 import static devgraft.common.URLPrefix.VERSION_1_PREFIX;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 
 @ExtendWith(RestDocumentationExtension.class)
-@WebMvcTest(AskFollowApi.class)
-public class AskFollowApiDoc extends AbstractApiDocTest {
+@WebMvcTest(CancelFollowApi.class)
+public class CancelFollowApiDoc extends AbstractApiDocTest {
     @MockBean
     private AuthCodeFilter authCodeFilter;
     @MockBean
-    private AskFollowService askFollowService;
+    private CancelFollowService cancelFollowService;
     @MockBean
     private MemberCredentialsResolver memberCredentialsResolver;
 
-    @DisplayName("팔로우 요청")
+    @DisplayName("팔로우 취소")
     @Test
-    void askFollow() throws Exception {
+    void cancelFollow() throws Exception {
         given(memberCredentialsResolver.resolveArgument(any(), any(), any(), any())).willReturn(MemberCredentials.builder().memberId("qwerty123").build());
 
         final AskFollowRequest givenRequest = AskFollowRequest.builder()
                 .followerLoginId("tom01")
                 .build();
 
-        mockMvc.perform(post(API_PREFIX + VERSION_1_PREFIX + FOLLOW_URL_PREFIX)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(getObjectMapper().writeValueAsString(givenRequest))
+        mockMvc.perform(delete(API_PREFIX + VERSION_1_PREFIX + FOLLOW_URL_PREFIX)
+                        .param("fId", "tom01")
                         .header("ACCESS-TOKEN", "accessToken")
                         .cookie(new Cookie("REFRESH-TOKEN", "refreshToken")))
                 .andDo(document.document(
-                        requestFields(
-                                fieldWithPath("followerLoginId").type(JsonFieldType.STRING).description("팔로우 대상 아이디")
+                        requestParameters(
+                                parameterWithName("fId").description("팔로우 취소 대상 아이디")
                         ),
                         responseFields
                 ))
