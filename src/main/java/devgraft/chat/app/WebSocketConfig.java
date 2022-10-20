50b91cfb30016a66a5f455515c75a69e36db1c31
PCloud <pcloud63514@gmail.com>
2022-10-20 21:15:24 +0900

    feat: WebSocket Test

--- src/main/java/devgraft/chat/app/WebSocketConfig.java
+++ src/main/java/devgraft/chat/app/WebSocketConfig.java
@@ -2,13 +2,11 @@ package devgraft.chat.app;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//@EnableWebSocketMessageBroker
+public class WebSocketConfig {//implements WebSocketMessageBrokerConfigurer {
 
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
