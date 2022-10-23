2bbc859f2cce1d7f1e0ce9daaf99b8fe068f02b1
PCloud <pcloud63514@gmail.com>
2022-10-23 18:59:50 +0900

    feat: WebSocket Test

--- src/main/java/devgraft/chat/domain/ChatRoom.java
+++ src/main/java/devgraft/chat/domain/ChatRoom.java
@@ -2,4 +2,7 @@ package devgraft.chat.domain;
 
 /**채팅방*/
 public class ChatRoom {
+    String id;
+    String issuedMemberId;
+    String createAt;
 }
--- src/main/java/devgraft/chat/domain/ChatRoomIn.java
+++ src/main/java/devgraft/chat/domain/ChatRoomIn.java
@@ -2,4 +2,8 @@ package devgraft.chat.domain;
 
 /**채팅방에 포함된 정보*/
 public class ChatRoomIn {
+    String chatRoomId;
+    String memberId;
+    String joinAt;
+    String status; // 참여 상태 { normal, leave }
 }
