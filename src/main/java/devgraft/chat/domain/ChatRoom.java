5e136f99fe784b6072c8a53a6a19113a66f7ce9f
PCloud <pcloud63514@gmail.com>
2022-10-24 19:13:48 +0900

    refactor: WebSocket delete 팔로우 이벤트 개발 후 진행할 예정

--- src/main/java/devgraft/chat/domain/ChatRoom.java
+++
@@ -1,8 +0,0 @@
-package devgraft.chat.domain;
-
-/**채팅방*/
-public class ChatRoom {
-    String id;
-    String issuedMemberId;
-    String createAt;
-}
--- src/main/java/devgraft/chat/domain/ChatRoomIn.java
+++
@@ -1,9 +0,0 @@
-package devgraft.chat.domain;
-
-/**채팅방에 포함된 정보*/
-public class ChatRoomIn {
-    String chatRoomId;
-    String memberId;
-    String joinAt;
-    String status; // 참여 상태 { normal, leave }
-}
